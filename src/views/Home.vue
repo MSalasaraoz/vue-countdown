e54869503d77fee0e4dd9546c9cb47deb850242f
Mariano Salas Araoz <marianosalasaraoz@gmail.com>
2021-06-30 10:52:50 -0700

    created countdown

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,25 @@
 <template>
   <div class="home">
-    <h1>Hello World</h1>
-    
+    <Counter 
+    :year= "2023"
+    :month= "6"
+    :date= "28"
+    :hour= "23"
+    :minute= "59"
+    :second= "59"
+    :milisecond= "31"
+     />
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-
+import Counter from '../components/Counter.vue';
 
 export default {
   name: "Home",
   components: {
-    
+    Counter,
   },
 };
 </script>
